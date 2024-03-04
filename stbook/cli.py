0655f18b3d205fc99e03cc8b44e246d3ca3d6bc7
Aziz Alto <iamaziz.alto@gmail.com>
2024-03-03 21:37:41 -0500

    streamlit share server

--- stbook/cli.py
+++ stbook/cli.py
@@ -4,7 +4,7 @@ import os
 def run_app():
     # Get the directory where the package is installed
     dir_path = os.path.dirname(os.path.realpath(__file__))
-    app_path = os.path.join(dir_path, 'app.py')
+    app_path = os.path.join(dir_path, '../app.py')
 
     try:
         subprocess.run(["streamlit", "run", app_path])
